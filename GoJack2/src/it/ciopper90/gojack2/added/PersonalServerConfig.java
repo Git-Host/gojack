3a1f3e767ddaf344564869dd13e0b27a68f18fac
ciopper90 <ciopper90@e011b907-1d4b-0e42-8fbc-7d53b2191603>
2013-02-18 10:58:42 +0000

    update2

---
+++ GoJack2/src/it/ciopper90/gojack2/added/PersonalServerConfig.java
@@ -0,0 +1,46 @@
+package it.ciopper90.gojack2.added;
+
+import it.ciopper90.gojack2.R;
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class PersonalServerConfig extends Activity {
+	private SharedPreferences prefs;
+	private EditText url;
+
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.personalserverconfig);
+		this.prefs = this.getSharedPreferences("Setting", Context.MODE_PRIVATE);
+		String textData = this.prefs.getString("url", "http://");
+
+		this.url = (EditText) this.findViewById(R.id.urlsp);
+		this.url.setText(textData);
+
+		Button b = (Button) this.findViewById(R.id.Salvasp);
+		b.setOnClickListener(new Button.OnClickListener() {
+
+			public void onClick(final View arg0) {
+				SharedPreferences.Editor editor = PersonalServerConfig.this.prefs.edit();
+				if (PersonalServerConfig.this.url.getText().toString().contains(" ")) {
+					editor.putString("url", PersonalServerConfig.this.url.getText().toString()
+							.replace(" ", ""));
+				} else {
+					editor.putString("url", PersonalServerConfig.this.url.getText().toString());
+				}
+				editor.commit();
+				Toast.makeText(PersonalServerConfig.this.getApplicationContext(),
+						"Salvato url predefinita", Toast.LENGTH_SHORT).show();
+				PersonalServerConfig.this.finish();
+			}
+		});
+	}
+
+}
---
+++ GoJack2/src/it/ciopper90/gojack2/utils/WSInterface.java
@@ -0,0 +1,59 @@
+package it.ciopper90.gojack2.utils;
+
+import it.ciopper90.gojack2.R;
+import it.ciopper90.gojack2.added.WorkServizio;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+public class WSInterface {
+
+	private static WorkServizio ws;
+	private static Spinner spinner;
+	private static SpinnerAdapter spinneradapter;
+	private static ArrayList<Servizio> service;
+
+	public static Spinner setSpinner(final Context context) {
+		ws = new WorkServizio(context);
+		service = ws.caricaServizio();
+		spinner = new Spinner(context);
+		ArrayList<String> lista = new ArrayList<String>();
+		for (int i = 0; i < service.size(); i++) {
+			lista.add(service.get(i).getName());
+		}
+		spinneradapter = new ArrayAdapter<String>(context, R.layout.textview, lista);
+		spinner.setAdapter(spinneradapter);
+		return spinner;
+	}
+
+	public static void saveService(final String to, final String selectedItem) {
+		ws.saveService(to, selectedItem);
+	}
+
+	public static ArrayList<Servizio> getService() {
+		return service;
+	}
+
+	public static int PositionServizioNumero(final String number) {
+		ArrayList<String> lista = new ArrayList<String>();
+		for (int i = 0; i < service.size(); i++) {
+			lista.add(service.get(i).getName());
+		}
+		int pos = 0;
+		String serv = ws.ServizioNumero(number);
+		if (serv != null) {
+			int a = lista.indexOf(serv);
+			pos = lista.indexOf(serv);
+		}
+		if (pos == -1) {
+			return 0;
+		} else {
+			return pos;
+		}
+	}
+
+}
